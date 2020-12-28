eac12dbb8944f2c0145f2bfa539c48df30124a7f
ZhangHuan0407 <2360950578@qq.com>
2020-12-29 00:20:22 +0800

    JP.MVC Version 0.1.0.0

--- JigsawPuzzleMVC/JigsawPuzzleMVC/Properties/AssemblyInfo.cs
+++ JigsawPuzzleMVC/JigsawPuzzleMVC/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JigsawPuzzleMVC")]
-[assembly: AssemblyCopyright("版权所有(C)  2020")]
+[assembly: AssemblyCopyright("ZhangHuan(C)  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // 你可以指定所有值，也可以让修订版本和内部版本号采用默认值，
 // 方法是按如下所示使用 "*":
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
